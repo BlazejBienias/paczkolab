48af1eb92110a9cf40e2cd93cda353398c572c0c
Błażej Bienias <blazej.bienias@gmail.com>
2018-03-10 15:18:55 +0100

    Database config

--- class/Address.php
+++ class/Address.php
@@ -2,6 +2,41 @@
 
 class Address implements Action
 {
+    private $id;
+    private $miasto;
+    private $kodPocztowy;
+    private $ulica;
+    private $numerMieszkania;
 
-    
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public static function load($id = null)
+    {
+        // TODO: Implement load() method.
+    }
+
+    public static function loadAll()
+    {
+        // TODO: Implement loadAll() method.
+    }
+
+    public static function setDb(Database $db)
+    {
+        // TODO: Implement setDb() method.
+    }
+
+
 }
--- class/Size.php
+++ class/Size.php
@@ -2,5 +2,39 @@
 
 class Size implements Action
 {
+    private $id;
+    private $rozmiar;
+    private $cena;
+
+
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public static function load($id = null)
+    {
+        // TODO: Implement load() method.
+    }
+
+    public static function loadAll()
+    {
+        // TODO: Implement loadAll() method.
+    }
+
+    public static function setDb(Database $db)
+    {
+        // TODO: Implement setDb() method.
+    }
 
 }
