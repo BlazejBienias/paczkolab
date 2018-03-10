87f711541bac24a62cd78488c149b16a2972817d
Paweł Płatek <placiu@gmail.com>
2018-03-10 15:20:43 +0100

    Parcel class

--- class/Parcel.php
+++ class/Parcel.php
@@ -2,5 +2,40 @@
 
 class Parcel implements Action
 {
-    
+    private $id;
+    private $nadawca;
+    private $rozmiar;
+    private $adres;
+
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public static function load($id = null)
+    {
+        // TODO: Implement load() method.
+    }
+
+    public static function loadAll()
+    {
+        // TODO: Implement loadAll() method.
+    }
+
+    public static function setDb(Database $db)
+    {
+        // TODO: Implement setDb() method.
+    }
+
+
 }
